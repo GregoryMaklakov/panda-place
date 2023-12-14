1bae17fb8ac9d906d52bd45f509845b27c6ccc56
Gregory <maklakov.grigory@gmail.com>
2023-12-14 17:07:02 +0100

    add NavbarItems&Item | 2 categories

---
+++ src/config/index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI Kits",
+    value: "ui_kits" as const,
+    featured: [
+      {
+        name: "Editor picks",
+        href: "#",
+        imageSrc: "/images/ui-kits/mixed.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/images/ui-kits/blue.jpg",
+      },
+      {
+        name: "Bestsellers",
+        href: "#",
+        imageSrc: "/images/ui-kits/purple.jpg",
+      },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "Favorite Icons Picks",
+        href: "#",
+        imageSrc: "/images/icons/picks.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/images/icons/new.jpg",
+      },
+      {
+        name: "Bestselling Icons",
+        href: "#",
+        imageSrc: "/images/icons/bestsellers.jpg",
+      },
+    ],
+  },
+];
