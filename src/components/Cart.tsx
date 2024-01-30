78066f7f5788416dc212e9d0315eae2aad9c6d78
Gregory <maklakov.grigory@gmail.com>
2024-01-30 14:54:31 +0100

    add server config[payload, get-payload, server]

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -5,15 +5,19 @@ import { Separator } from "./ui/separator";
 import {
   Sheet,
   SheetContent,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "./ui/sheet";
 import { formatPrice } from "@/lib/utils";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import Image from "next/image";
 
 const Cart = () => {
-  const itemCount = 1;
-
+  const itemCount = 0;
+  const fee = 1;
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex flex-center p-2">
@@ -42,13 +46,53 @@ const Cart = () => {
                 </div>
                 <div className="flex">
                   <span className="flex-1">Transaction fee</span>
-                  <span>{formatPrice(1)}</span>
+                  <span>{formatPrice(fee)}</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Total</span>
+                  <span>{formatPrice(fee)}</span>
                 </div>
               </div>
+              <SheetFooter>
+                <SheetTrigger asChild>
+                  <Link
+                    href="/cart"
+                    className={buttonVariants({
+                      className: "w-full capitalize",
+                    })}
+                  >
+                    Continue to checkout
+                  </Link>
+                </SheetTrigger>
+              </SheetFooter>
             </div>
           </>
         ) : (
-          <div></div>
+          <div className="flex h-full flex-col items-center justify-center space-y-1">
+            <div
+              aria-hidden="true"
+              className="relative mb-4 h-60 w-60 text-muted-foreground"
+            >
+              <Image
+                src="/images/empty-cart.png"
+                fill
+                alt="empty shopping cart"
+              />
+            </div>
+            <div className="text-xl font-semibold">Your cart is empty</div>
+            <SheetTrigger asChild>
+              <Link
+                href="/products"
+                className={buttonVariants({
+                  variant: "link",
+                  size: "sm",
+                  className: "text-sm text-muted-foreground",
+                })}
+              >
+                Add items to your cart to checkout
+              </Link>
+            </SheetTrigger>
+          </div>
         )}
       </SheetContent>
     </Sheet>
